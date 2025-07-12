5bd84632bf2830c89ef5da8f22d65928ffa0e9f1
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-07-12 12:18:30 +0000

    fix: style coding according best practices

--- src/front/hooks/useGlobalReducer.jsx
+++ src/front/hooks/useGlobalReducer.jsx
@@ -2,9 +2,11 @@
 import { useContext, useReducer, createContext } from "react";
 import storeReducer, { initialStore } from "../store"  // Import the reducer and the initial state.
 
+
 // Create a context to hold the global state of the application
 // We will call this global state the "store" to avoid confusion while using local states
 const StoreContext = createContext()
+
 
 // Define a provider component that encapsulates the store and warps it in a context provider to 
 // broadcast the information throught all the app pages and components.
@@ -16,6 +18,7 @@ export function StoreProvider({ children }) {
         {children}
     </StoreContext.Provider>
 }
+
 
 // Custom hook to access the global state and dispatch function.
 export default function useGlobalReducer() {
--- src/front/pages/Demo.jsx
+++ src/front/pages/Demo.jsx
@@ -1,4 +1,3 @@
-// Import necessary components from react-router-dom and other parts of the application.
 import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
 
@@ -8,36 +7,9 @@ export const Demo = () => {
 
   return (
     <div className="container">
-      <ul className="list-group">
-        {/* Map over the 'todos' array from the store and render each item as a list element */}
-        {store && store.todos?.map((item) => {
-          return (
-            <li
-              key={item.id}  // React key for list items.
-              className="list-group-item d-flex justify-content-between"
-              style={{ background: item.background }}> 
-              
-              {/* Link to the detail page of this todo. */}
-              <Link to={"/single/" + item.id}>Link to: {item.title} </Link>
-              
-              <p>Open file ./store.js to see the global store that contains and updates the list of colors</p>
-              
-              <button className="btn btn-success" 
-                onClick={() => dispatch({
-                  type: "add_task", 
-                  payload: { id: item.id, color: '#ffa500' }
-                })}>
-                Change Color
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <h1 className="text-center text-primary">Demo</h1>
       <br />
-
-      <Link to="/">
-        <button className="btn btn-primary">Back home</button>
-      </Link>
+      <Link to="/" className="btn btn-primary">Back home</Link>
     </div>
   );
 };
