1daedf52b36555044b1898758dd0ca106ca77d66
Valentina Gómez <valengogu96@gmail.com>
2025-07-17 17:38:42 +0000

    [routes] wip:favorites,messages

--- src/api/routes.py
+++ src/api/routes.py
@@ -18,3 +18,58 @@ def handle_hello():
 
 
 
+
+@api.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Messages.query.all()
+    return jsonify([msg.serialize() for msg in messages]), 200
+
+@api.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    msg = Messages(user_id=data.get('user_id'),
+                   user_to=data.get('user_to'),
+                   content=data.get('content'),
+                   created_at=data.get('created_at'),
+                   review_date=data.get('review_date'))
+    db.session.add(msg)
+    db.session.commit()
+    return jsonify(msg.serialize()), 201
+
+@api.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    msg = Messages.query.get(id)
+    if not msg:
+        return jsonify({"error": "Message not found"}), 404
+    db.session.delete(msg)
+    db.session.commit()
+    return jsonify({"msg": "Message deleted"}), 200
+
+@api.route('/favorites', methods=['GET'])
+def get_favorites():
+    favorites = Favorites.query.all()
+    return jsonify([fav.serialize() for fav in favorites]), 200
+
+@api.route('/favorites', methods=['POST'])
+def create_favorite():
+    data = request.get_json()
+    fav = Favorites(user_id=data.get('user_id'),
+                    product_id=data.get('product_id'))
+    db.session.add(fav)
+    db.session.commit()
+    return jsonify(fav.serialize()), 201
+
+@api.route('/favorites/<int:id>', methods=['DELETE'])
+def delete_favorite(id):
+    fav = Favorites.query.get(id)
+    if not fav:
+        return jsonify({"error": "Favorite not found"}), 404
+    db.session.delete(fav)
+    db.session.commit()
+    return jsonify({"msg": "Favorite deleted"}), 200
+
+
+
+
+
+
